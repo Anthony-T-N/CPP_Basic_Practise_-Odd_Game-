83be2829daf47c99ad1fd7b5d60e3a171aaf499f
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-06-02 19:58:24 +0930

    Update CPP_Basic_Practise_(Odd_Game).cpp

--- CPP_Basic_Practise_(Odd_Game).cpp
+++ CPP_Basic_Practise_(Odd_Game).cpp
@@ -1,40 +1,17 @@
-// -CPP_Basic_Practise_(Odd_Game)-.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 void board_generation(int size)
 {
     for (int i = 0; i <= size - 1; i++)
     {
-        std::cout << "[ ]";
+        for (int i = 0; i <= size - 1; i++)
+        {
+            std::cout << "[ ]";
+        }
+        std::cout << "\n";
     }
-    std::cout << "\n";
-    for (int i = 0; i <= size - 1; i++)
-    {
-        std::cout << "[ ]";
-    }
-    std::cout << "\n";
-    for (int i = 0; i <= size - 1; i++)
-    {
-        std::cout << "[ ]";
-    }
-    std::cout << "\n";
-    for (int i = 0; i <= size - 1; i++)
-    {
-        std::cout << "[ ]";
-    }
-    std::cout << "\n";
-    for (int i = 0; i <= size - 1; i++)
-    {
-        std::cout << "[ ]";
-    }
-    std::cout << "\n";
-    for (int i = 0; i <= size - 1; i++)
-    {
-        std::cout << "[ ]";
-    }
-    std::cout << "\n";
+
+    std::cout << "\n\n";
 }
 
 int main()
