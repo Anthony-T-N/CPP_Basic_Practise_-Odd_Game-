d20764c72e786dd53c83927ee4f0de79a237207d
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-06-03 19:05:54 +0930

    Update CPP_Basic_Practise_(Odd_Game).cpp

--- CPP_Basic_Practise_(Odd_Game).cpp
+++ CPP_Basic_Practise_(Odd_Game).cpp
@@ -4,10 +4,27 @@
 void board_generation(int size)
 {
     // std::vector == Dynamic array (Similar to C# lists)
+    // 2D Vectory array.
+    std::vector<std::vector<std::string>> main_board;
 
-    std::vector<std::string> board;
-    board.push_back({ "[ ]", "[ ]" , "[ ]" });
-
+    for (int i = 0; i < size; i++)
+    {
+        std::vector<std::string> board_sector;
+        for (int j = 0; j < size; j++)
+        {
+            board_sector.push_back("[ ]");
+        }
+        main_board.push_back(board_sector);
+    }
+    for (int i = 0; i < main_board.size(); i++)
+    {
+        for (int j = 0; j < main_board[i].size(); j++)
+        {
+            std::cout << main_board[i][j];
+        }
+        std::cout << "\n";
+    }
+    /*
     for (int i = 0; i <= size - 1; i++)
     {
         for (int i = 0; i <= size - 1; i++)
@@ -17,11 +34,12 @@ void board_generation(int size)
         std::cout << "\n";
     }
     std::cout << "\n\n";
+    */
 }
 
 int main()
 {
     std::cout << "Welcome friend!" << "\n\n";
-    board_generation(10);
+    board_generation(5);
     return 0;
 }
