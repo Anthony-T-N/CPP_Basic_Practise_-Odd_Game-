57221a5890c1a71af3f5e59d684be7a2e86c537d
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-06-04 19:35:39 +0930

    Update CPP_Basic_Practise_(Odd_Game).cpp

--- CPP_Basic_Practise_(Odd_Game).cpp
+++ CPP_Basic_Practise_(Odd_Game).cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 
-void board_generation(int size)
+// std::vector == Dynamic array (Similar to C# lists)
+// 2D Vectory array.
+std::vector<std::vector<std::string>> main_board;
+
+void board_generation(int &size)
 {
-    // std::vector == Dynamic array (Similar to C# lists)
-    // 2D Vectory array.
-    std::vector<std::vector<std::string>> main_board;
-
+    std::vector<std::string> board_sector;
+   
+    /*
+    for (int j = 0; j < size; j++)
+    {
+        board_sector.push_back("[" + std::to_string(j + 1) + "]");
+    }
+    main_board.push_back(board_sector);
+    */
     for (int i = 0; i < size; i++)
     {
-        std::vector<std::string> board_sector;
+        board_sector.clear();
+        // board_sector.push_back("[" + std::to_string(i + 1) + "]");
         for (int j = 0; j < size; j++)
         {
             board_sector.push_back("[ ]");
         }
         main_board.push_back(board_sector);
     }
+    // Print board.
     for (int i = 0; i < main_board.size(); i++)
     {
         for (int j = 0; j < main_board[i].size(); j++)
@@ -24,22 +36,28 @@ void board_generation(int size)
         }
         std::cout << "\n";
     }
-    /*
-    for (int i = 0; i <= size - 1; i++)
-    {
-        for (int i = 0; i <= size - 1; i++)
-        {
-            std::cout << "[ ]";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n\n";
-    */
+}
+
+void item_positioning()
+{
+    main_board[4][7] = "[@]";
 }
 
 int main()
 {
     std::cout << "Welcome friend!" << "\n\n";
-    board_generation(5);
+    int board_size;
+    std::cin >> board_size;
+    board_generation(board_size);
+    std::cout << "\n\n";
+    item_positioning();
+    for (int i = 0; i < main_board.size(); i++)
+    {
+        for (int j = 0; j < main_board[i].size(); j++)
+        {
+            std::cout << main_board[i][j];
+        }
+        std::cout << "\n";
+    }
     return 0;
 }
