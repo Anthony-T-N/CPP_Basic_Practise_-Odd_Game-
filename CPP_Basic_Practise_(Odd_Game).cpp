9a84464bbb3fda41685047d3f73724d7380f6838
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-06-05 16:35:43 +0930

    Update CPP_Basic_Practise_(Odd_Game).cpp

--- CPP_Basic_Practise_(Odd_Game).cpp
+++ CPP_Basic_Practise_(Odd_Game).cpp
@@ -5,12 +5,15 @@
 // std::vector == Dynamic array (Similar to C# lists)
 // 2D Vectory array.
 std::vector<std::vector<std::string>> main_board;
+// Current position of player.
+int position_one, position_two;
 
 void board_generation(int &size)
 {
     std::vector<std::string> board_sector;
    
     /*
+    * // Adds row of number on top of board.
     for (int j = 0; j < size; j++)
     {
         board_sector.push_back("[" + std::to_string(j + 1) + "]");
@@ -38,19 +41,81 @@ void board_generation(int &size)
     }
 }
 
-void item_positioning()
+void item_positioning(int size)
 {
-    main_board[4][7] = "[@]";
+    // Generate number between 0 and size.
+    int random_pos_one = rand() % (size);
+    int random_pos_two = rand() % (size);
+    std::cout << random_pos_one << "|" << random_pos_two << "\n\n";
+    main_board[random_pos_one][random_pos_two] = "[@]";
+    position_one = random_pos_one;
+    position_two = random_pos_two;
+    for (int i = 0; i <= 5; i++)
+    {
+        main_board[rand() % (size)][rand() % (size)] = "[#]";
+        main_board[rand() % (size)][rand() % (size)] = "[!]";
+        main_board[rand() % (size)][rand() % (size)] = "[%]";
+    }
+}
+
+void player_control(std::string& user_input)
+{
+    // Move left.
+    if (user_input == "w")
+    {
+        // Remove main character from current position.
+        main_board[position_one][position_two] = "[ ]";
+        // Move main character on board up by one.
+        position_one -= 1;
+        main_board[position_one][position_two] = "[@]";
+    }
+    // Move down.
+    else if (user_input == "s")
+    {
+        // Remove main character from current position.
+        main_board[position_one][position_two] = "[ ]";
+        // Move main character on board down by one.
+        position_one += 1;
+        main_board[position_one][position_two] = "[@]";
+    }
+    // Move right.
+    else if (user_input == "d")
+    {
+        // Remove main character from current position.
+        main_board[position_one][position_two] = "[ ]";
+        // Move main character on board down by one.
+        position_two += 1;
+        main_board[position_one][position_two] = "[@]";
+    }
+    // Move up.
+    else if (user_input == "a")
+    {
+        // Remove main character from current position.
+        main_board[position_one][position_two] = "[ ]";
+        // Move main character on board down by one.
+        position_two -= 1;
+        main_board[position_one][position_two] = "[@]";
+    }
+    else if (user_input == "exit")
+    {
+        return;
+    }
+    else
+    {
+        std::cout << "[-] Invalid User Input;" << "\n";
+    }
+
 }
 
 int main()
 {
+    // Never trust user input.
     std::cout << "Welcome friend!" << "\n\n";
     int board_size;
     std::cin >> board_size;
     board_generation(board_size);
     std::cout << "\n\n";
-    item_positioning();
+    item_positioning(board_size);
     for (int i = 0; i < main_board.size(); i++)
     {
         for (int j = 0; j < main_board[i].size(); j++)
@@ -58,6 +123,20 @@ int main()
             std::cout << main_board[i][j];
         }
         std::cout << "\n";
+    }
+    std::string user_input;
+    while (true)
+    {
+        std::cin >> user_input;
+        player_control(user_input);
+        for (int i = 0; i < main_board.size(); i++)
+        {
+            for (int j = 0; j < main_board[i].size(); j++)
+            {
+                std::cout << main_board[i][j];
+            }
+            std::cout << "\n";
+        }
     }
     return 0;
 }
